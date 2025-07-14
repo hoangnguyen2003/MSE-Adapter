0505e0558199ee5fe86df045b73c41cda5b868f0
hoangnguyen2003 <ninjadoc0152@gmail.com>
2025-07-14 23:57:26 +0700

    minor changes

--- MSE-Llama2-7B/run.py
+++ MSE-Llama2-7B/run.py
@@ -102,8 +102,6 @@ def run(args):
 
 
 def run_normal(args):
-    print(1111)
-
     args.res_save_dir = os.path.join(args.res_save_dir)
     init_args = args
     model_results = []
@@ -200,7 +198,7 @@ def parse_args():
                         help='path to save results.')
     parser.add_argument('--pretrain_LM', type=str, default='/kaggle/input/chatglm3-6b',
                         help='path to load pretrain LLM.')
-    parser.add_argument('--gpu_ids', type=list, default=[2],
+    parser.add_argument('--gpu_ids', type=list, default=[0],
                         help='indicates the gpus will be used. If none, the most-free gpu will be used!')   #使用GPU1
     return parser.parse_args()
 
